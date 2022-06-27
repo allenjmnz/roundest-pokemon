a8573a54625af7898bbb8179285524fe157b5d66
Allen Jiménez <allenjimenez@lyft.com>
2022-06-26 19:58:45 -0500

    Initial UI structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,16 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="h-screen w-screen flex flex-col justify-center items-center">
+      <div className="text-2xl text-center">Which Pokémon is rounder?</div>
+      <div className="p-2" />
+      <div className="border rounded p-8 flex justify-between items-center max-w-2xl">
+        <div className="w-16 h-16 bg-red-200"></div>
+        <div className="p-8">vs</div>
+        <div className="w-16 h-16 bg-red-200"></div>
+        <div></div>
+      </div>
+    </div>
   )
 }
 
